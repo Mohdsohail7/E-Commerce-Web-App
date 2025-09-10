12a25925c24558d3ce50fe27b8d844e0477dbe26
Mohdsohail7 <sohailmohd437@gmail.com>
2025-09-10 17:09:16 +0530

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+
+dotenv.config();
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+// default route
+app.get("/", (req, res => {
+    res.json({ message: "E-Commerce-backend web app is running..."});
+}));
+
+const port = process.env.PORT || 4000;
+connectDB().then(() => {
+    app.listen(port, () => {
+        console.log(`Server is running at port: ${port}`);
+    })
+})
